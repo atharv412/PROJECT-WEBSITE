f3f5ce4bfcef8b9571b543d5cfe38f5a3c9492fc
Atharva Sonawane <atharva.sonawane04@gmail.com>
2023-12-31 18:56:05 +0530

    created responsive nav-bar and added a basic hero image

---
+++ Script3.js
@@ -0,0 +1,8 @@
+function showSidebar() {
+    const sidebar=document.querySelector('#side-bar')
+    sidebar.style.display='flex'
+}
+function closeSideBar() {
+    const sidebar=document.querySelector('#side-bar')
+    sidebar.style.display='none'
+}
