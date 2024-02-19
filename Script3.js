cca1a307a5293ddcf14f94cc88000a21bad0045e
Atharva Sonawane <atharva.sonawane04@gmail.com>
2024-02-19 23:47:22 +0530

    updates
    -created order page
    -filled in the information for remaining products with images(for descriptions )
    -attached nav bar and footer

--- Script3.js
+++ Script3.js
@@ -1,9 +1,24 @@
-function showSidebar() {
+function showSidebar() 
+{
     const sidebar=document.querySelector('#side-bar')
     sidebar.style.display='flex'
 }
-function closeSideBar() {
+function closeSideBar()
+{
     const sidebar=document.querySelector('#side-bar')
     sidebar.style.display='none'
 }
-
+function showWholesale()
+{
+    const wholesale=document.querySelector("#order-type1")
+    const bulk=document.querySelector('#order-type2')
+    bulk.style.display='none'
+    wholesale.style.display='block'
+}
+function showBulk()
+{
+    const wholesale=document.querySelector("#order-type1")
+    const bulk=document.querySelector('#order-type2')
+    bulk.style.display='block'
+    wholesale.style.display='none'
+}  
